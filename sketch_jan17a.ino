e0e32863f846b116bfdbb9dbd87ffe3d3bd01501
Teamon234 <97162625+Teamon234@users.noreply.github.com>
2022-01-20 10:42:58 +0100

    Update sketch_jan17a.ino

--- sketch_jan17a.ino
+++ sketch_jan17a.ino
@@ -1,12 +1,6 @@
 #define BLYNK_TEMPLATE_ID "TMPLWhBrHvx5"
 #define BLYNK_DEVICE_NAME "Quickstart Template"
 #define BLYNK_AUTH_TOKEN "lLFhu2YOiduw7VPpGll1sui2AT2JKdPd"
-// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
-// See the Device Info tab, or Template settings
-//#define BLYNK_TEMPLATE_ID           "TMPLlVYrlAl9"
-//#define BLYNK_DEVICE_NAME           "Quickstart Device"
-//#define BLYNK_AUTH_TOKEN            "Mg6Mne_iLT2jkquxwCgV6iVqMVv4qCAk"
-
 
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
@@ -25,8 +19,6 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "iPhone von Timon";
 char pass[] = "felix123";
 
-BlynkTimer timer;
-
 void setup()
 {
   // Debug console
@@ -38,33 +30,12 @@ void setup()
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 
   // Setup a function to be called every second
-  timer.setInterval(1000L, myTimerEvent);
+  
   pinMode(ledPin, OUTPUT);
   pinMode(gndPin, OUTPUT);
+  digitalWrite(gndPin,LOW);
 } 
 
-void loop()
-{
-  Blynk.run();
-  timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
-  }
-
-// This function is called every time the Virtual Pin 0 state changes
-BLYNK_WRITE(V0)
-{
-  // Set incoming value from pin V0 to a variable
-  int value = param.asInt();
-
-  // Update state
-  Blynk.virtualWrite(V1, value);
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin,LOW);
-  delay(1000);
-}
 
 // This function is called every time the device is connected to the Blynk.Cloud
 BLYNK_CONNECTED()
@@ -75,44 +46,23 @@ BLYNK_CONNECTED()
 
 }
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, millis() / 1000);
-}
 
 BLYNK_WRITE(V0)
 {
   if(param.asInt() ==1)
   {
-    digitalWrite(D1,HIGH);
-    digitalWrite(D2,LOW);
-    {
+    digitalWrite(ledPin,HIGH);
+    
+  }
     else
     {
-      digitalWrite(D1,LOW);
-      digitalWrite(D2,LOW);  
+      digitalWrite(ledPin,LOW);
+      
   }
 }
-BLYNK_WRITE(V5)
-{
-  if(param.asInt() ==1)
-  {
-    digitalWrite(D1,HIGH);
-    digitalWrite(D2,LOW);
-    Blynk.virtualWrite(V1,1);
-    Blynk.virtualWrite(V2,1);
-    Blynk.virtualWrite(V3,1);
-    Blynk.virtualWrite(V4,1);
-    {
-    else
-    {
-      digitalWrite(D1,LOW);
-      digitalWrite(D2,LOW);
 
+void loop()
 {
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite
+  Blynk.run();
+  
+  }
